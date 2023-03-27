1e5eb8408d2888aedfe1a2bda26d39fb547e763b
nurullah91 <mdnurullah.ptk@gmail.com>
2023-03-28 01:33:31 +0600

    cart summery added

--- src/component/Shop/Shop.jsx
+++ src/component/Shop/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Cart from '../Product/Cart/Cart';
 import Product from '../Product/Product';
 import "./Shop.css"
 
@@ -30,9 +31,7 @@ const Shop = () => {
                }
             </div>
             <div className="card-container">
-                <h4>Order summery</h4>
-
-                <p>Selected Items: {cart.length}</p>
+               <Cart cart = {cart}></Cart>
             </div>
         </div>
     );
