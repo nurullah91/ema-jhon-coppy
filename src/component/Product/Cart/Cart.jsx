f01af2c47ee50e1ba6b711254c1485f14acff159
nurullah91 <mdnurullah.ptk@gmail.com>
2023-03-28 12:56:14 +0600

    added commented code

--- src/component/Product/Cart/Cart.jsx
+++ src/component/Product/Cart/Cart.jsx
@@ -15,7 +15,8 @@ const Cart = ({ cart }) => {
         // }
         
         // shortcut way;
-        product.quantity = product.quantity || 1;
+        // product.quantity = product.quantity || 1;
+        
         totalPrice = totalPrice + product.price * product.quantity;
 
         totalShipping = totalShipping + product.shipping;
