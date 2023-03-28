f68cb317a5ab509cb2848be323ef54023440b458
nurullah91 <mdnurullah.ptk@gmail.com>
2023-03-28 12:53:30 +0600

    all done from 51 module

--- src/component/Shop/Shop.jsx
+++ src/component/Shop/Shop.jsx
@@ -39,14 +39,31 @@ const Shop = () => {
             }
         }
 
-            // step-6: set the cart;
-            setCart(savedCart);
+        // step-6: set the cart;
+        setCart(savedCart);
 
 
     }, [products])
 
     const handleAddToCart = (product) => {
-        const newCart = [...cart, product];
+        // const newCart = [...cart, product];
+
+        // if product don't exist in the cart then set quantity = 1 and if exist update quantity by 1;
+        let newCart = [];
+        const exist = cart.find(pd => pd.id === product.id);
+
+        if(!exist){
+            product.quantity = 1;
+            newCart = [...cart, product] 
+        }
+        else{
+            exist.quantity = exist.quantity + 1;
+
+            const remaining = cart.filter(pd => pd.id !==product.id);
+            newCart = [...remaining, exist];
+        }
+
+
         setCart(newCart);
         addToDb(product.id)
     }
