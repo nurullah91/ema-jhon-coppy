a3633cb31a35d7af46ff5b0f41b8591cf0654260
nurullah91 <mdnurullah.ptk@gmail.com>
2023-04-05 03:23:22 +0600

    delete button implimented

--- src/component/ReviewItem/ReviewItem.jsx
+++ src/component/ReviewItem/ReviewItem.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import './ReviewItem.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 
-const ReviewItem = ({product}) => {
-const {id, img, name, price, quantity} = product;
+const ReviewItem = ({ product, handleRemoveFormCart }) => {
+    const { id, img, name, price, quantity } = product;
     return (
         <div className='review-item'>
-           <img src={img} alt="" />
+            <img src={img} alt="" />
+            <div className="review-details">
+                <p className='product-title'>{name}</p>
+                <p>price: <span className='orange-text'>${price}</span></p>
+                <p>Order Quantity: <span className='orange-text'>${quantity}</span></p>
+            </div>
+
+            <button onClick={ () =>handleRemoveFormCart(id)} className='dlt-btn'>
+            <FontAwesomeIcon className='dlt-icon' icon={faTrashAlt} />
+            </button>
         </div>
     );
 };
